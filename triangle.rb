0b5c11be91be9f0a7001ce5c2ea1a74716543b7c
Ernane Ferreira <ernane.junior25@gmail.com>
2021-06-16 15:23:16 -0300

    Triangle Project Code.

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b && a == c
+    return :equilateral
+  elsif (a == b and b != c) or (a == c and c != b) or (b == c and a != c)
+    return :isosceles
+  elsif a != b && a != c && b != c
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
